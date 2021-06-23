cc320ca133816b53d5786958f1f1656a867cb047
Purva Tayde <72730647+purva1908@users.noreply.github.com>
2021-06-23 15:40:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+console.log(numberOfDrumButtons);
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    console.log(buttonInnerHTML);
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "d":
+      var dog = new Audio("sounds/dog.wav");
+      dog.play();
+      break;
+
+    case "c":
+      var cat = new Audio("sounds/cat.wav");
+      cat.play();
+      break;
+
+    case "h":
+      var horse = new Audio('sounds/horse.wav');
+      horse.play();
+      break;
+
+    case "m":
+      var monkey = new Audio('sounds/monkey.wav');
+      monkey.play();
+      break;
+
+    case "p":
+      var parrot = new Audio('sounds/parrot.wav');
+      parrot.play();
+      break;
+
+    case "s":
+      var sparrow = new Audio('sounds/sparrow.mp3');
+      sparrow.play();
+      break;
+
+   
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
